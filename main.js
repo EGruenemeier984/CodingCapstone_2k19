a85bfa47d3491745edc1d5348c0a6f5fd94bbe6e
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-05-13 13:37:54 -0700

    Added comments for JS & CSS
    Added descriptive comments to each significant block of code for both the JavaScript and CSS.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// This sets the an array for all the options that show up in the selection list.
 var options = ["Request a Tour", "Request Information", "Other"];
-
+// This checks if the id of form_need is on the page then runs the function.
 if ($('#form_need').length > 0) {
     $(function () {
+        // This function creates a var for collect and loops through the options array adding the options to the inner HTML of the id.
         var collect = "";
         for (var i = 0; i < options.length; i++) {
             collect += `<option value=${i}> ${options[i]}</option>`
@@ -9,9 +11,9 @@ if ($('#form_need').length > 0) {
         document.getElementById("form_need").innerHTML = collect;
     });
 }
-
+// This creates a var of btn and sets it to the id of topButton
 var btn = $('#topButton');
-
+// This function is triggered when the window is scrolled and adds the show class and removes it.
 $(window).scroll(function () {
     if ($(window).scrollTop() > 200) {
         btn.addClass('show');
@@ -19,7 +21,7 @@ $(window).scroll(function () {
         btn.removeClass('show');
     }
 });
-
+// This sets the page position when the scroll top button is selected and uses jquery's animate function.
 btn.on('click', function (e) {
     e.preventDefault();
     $("html, body").animate({
@@ -27,7 +29,7 @@ btn.on('click', function (e) {
     }, "slow");
     return false;
 });
-
+// This function is for the mobile slideout menu that opens up the menu when the button is clicked
 $(function () {
     $("#menu-toggle").click(function (e) {
         e.preventDefault();
@@ -44,22 +46,24 @@ $(function () {
 });
 
 // CountDown Code
+// This is the code for the countdown timer and defines the length for seconds, minutes, hours, and days.
 if ($('#countdown').length > 0) {
     const second = 1000,
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
-
+    // sets the date to countdown to
     let countDown = new Date("August 1, 2019").getTime();
+    // uses a function as an object to set an interval then sets the value to each id.
     x = setInterval(function () {
         let now = new Date().getTime();
         distance = countDown - now;
 
-        document.getElementById("days").textContent = Math.floor(distance / (day));
+        document.getElementById("days").innerText = Math.floor(distance / (day));
         document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
         document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
         document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / (second));
-
+        // This is the check for when the distance to the time is 0 and then it displays the final text.
         if (distance <= 0) {
             clearInterval(x);
             document.getElementById("#countH").innerText = "It's time for WELDING!"
